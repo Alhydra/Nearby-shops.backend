d7d764e0d36efff47be3a55d1e526a66e6dd77be
Youssef El Habchi <ellhydra1@gmail.com>
2018-11-19 21:29:27 +0000

    Add user Router

---
+++ app/Routes/userRouter.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const userRouter = express.Router()
+const User = require("../models/User.js")
+
+
+// ADD ONE USER
+userRouter.post("/",(req,res)=>{
+
+    console.log("user post");
+    
+    const user = new User()
+    user.email = req.body.email
+    user.password = req.body.password
+
+    user.save((err)=>{
+        if(err){
+            res.send(err.message)
+        }else{
+            res.send({status:"User added",user})
+        }
+    })
+})
+
+
+module.exports = userRouter
+
