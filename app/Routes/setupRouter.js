29a33a1ac878c914bc73f99692a4143fcd72652e
Youssef El Habchi <ellhydra1@gmail.com>
2018-11-21 14:56:06 +0000

    Bug Fix: set up null records

--- app/Routes/setupRouter.js
+++ app/Routes/setupRouter.js
@@ -12,11 +12,11 @@ setupRouter.get("/",(req,res)=>{
         {name:"Megamall",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-s/05/35/fb/76/megamall.jpg",lat:34.032405,lng:-6.934543,likes:["ellhydra1@gmail.com","ellhydra2@gmail.com"]},
         {name:"Au Grain de Sesame",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-f/0a/f4/7b/aa/espace-galerie-art-et.jpg",lat:34.052405,lng:-6.814543,likes:[]},
         {name:"Cooperative Ibdaa",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-f/05/b3/f5/46/cooperative-ibdaa.jpg",lat:34.092405,lng:-6.734543,likes:[]},
-        {name:"Apia Huiles et Miels",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-i/10/6d/ce/0c/apia-huiles-et-miels.jpg",lat:34.122405,lng:-6.834543,likes:["ellhydra1@gmail.com",,"ellhydra2@gmail.com"]},
+        {name:"Apia Huiles et Miels",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-i/10/6d/ce/0c/apia-huiles-et-miels.jpg",lat:34.122405,lng:-6.834543,likes:["ellhydra1@gmail.com","ellhydra2@gmail.com"]},
         {name:"Abla Ababou Galerie",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-i/11/03/eb/8b/facade-de-la-galerie.jpg",lat:34.022405,lng:-6.634543,likes:[]},
         {name:"Dune Galerie",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-f/0b/b4/86/01/img-20160623-094151-largejpg.jpg",lat:34.022405,lng:-6.834543,likes:[]},
         {name:"Aux Merveilles de Marrakech",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-f/06/cd/05/fd/aux-merveilles-de-marrakech.jpg",lat:36.022405,lng:-6.034543,likes:[]},
-        {name:"Ensemble Artisanal",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-f/05/c1/5c/33/ensemble-artisanal.jpg",lat:34.022405,lng:-7.034543},
+        {name:"Ensemble Artisanal",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-f/05/c1/5c/33/ensemble-artisanal.jpg",lat:34.022405,lng:-7.034543,likes:[]},
         {name:"33 Rue Majorelle",imageUrl:"https://media-cdn.tripadvisor.com/media/photo-s/02/62/b4/b9/overview.jpg",lat:34.022405,lng:-8.834543,likes:[]},
     
     
